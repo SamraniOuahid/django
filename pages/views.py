2fc9d1c2e69b1d7cfe8b4a070d376ea26c713bd3
ouahidsamrani <lshdkds@gmail.com>
2024-11-30 18:16:56 +0000

    ss1

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from .models import Male
 # Create your views here.
 def index(request):
-    return HttpResponse('hello to index')
+    return render(request, 'pages/index.html')
 def pages(request):
-    return render(request, 'pages/pages.html', {'pro': Male.objects.get(name_male='ouahid')})
+    return render(request, 'pages/pages.html', {'pro': Male.objects.get(name_male='oussama')})
