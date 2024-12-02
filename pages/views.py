e4738c10c30a675182f2c49aa88ba9239e345412
ouahidsamrani <lshdkds@gmail.com>
2024-12-02 23:18:55 +0000

    edit1

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Male
+from .models import Male, Female
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', {'name':'rachid'})
 def pages(request):
-    return render(request, 'pages/pages.html', {'pro': Male.objects.get(name_male='oussama')})
+    return render(request, 'pages/about.html', {'n1':'ouahid'} )
+
+#{'pro': Male.objects.get(name_male='oussama')}#
